11869e88b60dd904f3acc5600d3b35eb905375b0
pingzing <neil@travelneil.com>
2025-03-16 16:45:03 +0200

    Add song label, separator, and guidelines

--- main.py
+++ main.py
@@ -3,8 +3,36 @@ from tkinter import ttk
 
 root = tk.Tk()
 root.title("YouTube Music Downloader")
+root.minsize(width=350, height=50)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
-content = ttk.Frame(root, padding=(5,5,5,5), width=350, height=50)
-content.pack()
+# Main content frame, contains everything
+content = ttk.Frame(root, padding=(5, 5, 5, 5))
+content.grid(row=0, column=0, sticky=(tk.N, tk.E, tk.W, tk.S))
+
+# Grid Row 0: Label & spinbox
+
+song_label = ttk.Label(content, text="Songs")
+song_label.grid(row=0, column=0, stick=(tk.E))
+
+song_counter_picker = ttk.Spinbox(content, from_=0, to=20, wrap=True, width=5)
+song_counter_picker.set(0)
+song_counter_picker.grid(row=0, column=1, sticky=(tk.W))
+
+content.grid_rowconfigure(0, weight=1)
+content.grid_columnconfigure(0, weight=1)
+content.grid_columnconfigure(1, weight=1)
+
+# Grid Row 1: Separator
+separator = ttk.Separator(content, orient=tk.HORIZONTAL)
+separator.grid(row=1, column=0, columnspan=2, sticky=(tk.E, tk.W))
+
+# Grid Row 2: Frame, scrollbar
+# Frame's grid has rows of:
+# Editbox, Label
+
+list_container = ttk.Frame(content, padding=(5, 5, 5, 5))
+list_container.grid(row=1, column=0)
 
 root.mainloop()
