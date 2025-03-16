2ca2c10c7d266dbc77aa91bca88ec40b8088dba4
pingzing <neil@travelneil.com>
2025-03-16 18:00:11 +0200

    Simplify significantly, fix row ordering

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
+import itertools
 
 root = tk.Tk()
 song_counter_value = tk.DoubleVar()
 
 list_container_rows = 0
-list_container = None
+list_container: ttk.Frame = None
 
 def main():
     root.title("YouTube Music Downloader")
@@ -17,57 +18,52 @@ def main():
     content = ttk.Frame(root, padding=(5, 5, 5, 5))
     content.grid(row=0, column=0, sticky=(tk.N, tk.E, tk.W, tk.S))
 
-    # Grid Row 0: Label & spinbox
+    # Grid Row 0: Label, spinbox, button
 
     song_label = ttk.Label(content, text="Songs")
     song_label.grid(row=0, column=0, stick=(tk.E))
 
     song_counter_value.set(0)
-    song_counter_value.trace_add("write", on_value_changed)
     song_counter_picker = ttk.Spinbox(
         content, from_=0, to=20, wrap=True, width=5, textvariable=song_counter_value)
     song_counter_picker.set(0)
     song_counter_picker.grid(row=0, column=1, sticky=(tk.W))
 
-    content.grid_rowconfigure(0, weight=20)
+    create_button = ttk.Button(content, text="Create", command=generate_list)
+    create_button.grid(row=0, column=2, sticky=(tk.E))
+
+    content.grid_rowconfigure(0, weight=10)
     content.grid_columnconfigure(0, weight=100)
     content.grid_columnconfigure(1, weight=100)
 
     # Grid Row 1: Separator
     separator = ttk.Separator(content, orient=tk.HORIZONTAL)
-    separator.grid(row=1, column=0, columnspan=2, sticky=(tk.E, tk.W))
+    separator.grid(row=1, column=0, columnspan=3, sticky=(tk.E, tk.W))
+
+    content.grid_rowconfigure(1, weight=10)
 
     # Grid Row 2: Frame, scrollbar
     # Frame's grid has rows of:
     # Editbox, Label
 
+    global list_container
     list_container = ttk.Frame(content, padding=(5, 5, 5, 5))
-    list_container.grid(row=1, column=0)
+    list_container.grid(row=2, column=0)
 
-    content.grid_rowconfigure(1, weight=80)
+    content.grid_rowconfigure(2, weight=80)
 
     root.mainloop()
 
 
-def on_value_changed(var, index, mode):
-    new_val = song_counter_value.get()
-    update_list(new_val)
+def generate_list():
+    new_count = int(song_counter_value.get())
 
+    for list_item in list_container.grid_slaves():
+        list_item.grid_remove()
 
-def update_list(new_count):
-    if (list_container == None):
-        return
-
-    if (new_count == list_container_rows):
-        return
-
-    difference = new_count - list_container_rows
-    if (difference < 0):
-        # remove rows
-        raise NotImplementedError()
-    else:
-        # add rows
-        raise NotImplementedError()
+    for i in range(0, new_count):
+        label = ttk.Label(list_container, text=f"Test {i}")
+        label.grid(row=i, column=0)
 
 
 main()
